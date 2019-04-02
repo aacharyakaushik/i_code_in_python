eda7b547e57d856935308ddc67faae33ed54e469
aacharyakaushik <aacharyakaushik@gmail.com>
2019-04-02 12:04:19 -0700

    small changes in graph titles

--- Generate_graph.py
+++ Generate_graph.py
@@ -3,9 +3,9 @@ x = [7, 12, 3303, 3844, 11383,155462, 1078176]
 y = [0.005859, 0.010010, 2.839111, 3.369141, 10.299805,	136.911865,	1083.556396 ]
 
 plt.plot(x,y,marker = 'o')
-plt.xlabel('')
-plt.ylabel('')
-plt.title('')
+plt.xlabel('Input Size')
+plt.ylabel('Construction Time')
+plt.title('Progression of Construction Time wrt Input Size')
 plt.savefig("CT_CG.PNG")
 plt.close()
 
@@ -15,7 +15,7 @@ plt.xticks(rotation = 45)
 plt.figure(figsize=(10,7))
 plt.plot(m,n,marker = 'o')
 plt.xlabel('Input Size')
-plt.ylabel('Implementation')
+plt.ylabel('Implementation Constant')
 plt.title('Progression of Implementation Constant wrt Input Size')
 
 for i,txt in enumerate (n):
