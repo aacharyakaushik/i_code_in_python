38be7620e7309678f01954fcd20388456255b993
aacharyakaushik <aacharyakaushik@gmail.com>
2020-02-23 14:22:01 -0800

    palindrome implementation

--- leetcode/Pal_number.py
+++ leetcode/Pal_number.py
@@ -5,20 +5,22 @@ Created on Tue Jan 21 12:10:50 2020
 @author: Kaushik Acharya
 """
 
+
 def isPalindrome(self, x: int) -> bool:
-    if x < 0 :
+    if x < 0:
         return False
     else:
-        if str(x)== str(x)[::-1]:
+        if str(x) == str(x)[::-1]:
             return True
-        else :
+        else:
             return False
-        
-p = isPalindrome(isPalindrome,121)
+
+
+p = isPalindrome(isPalindrome, 121)
 print(p)
 
-q = isPalindrome(isPalindrome,-121)
+q = isPalindrome(isPalindrome, -121)
 print(q)
 
-r = isPalindrome(isPalindrome,10)
+r = isPalindrome(isPalindrome, 10)
 print(r)
