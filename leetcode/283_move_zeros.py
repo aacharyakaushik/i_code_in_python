91aa07d0fcc3e94807587f4bdb54cbc6de895c4a
aacharyakaushik <aacharyakaushik@gmail.com>
2020-03-25 20:21:06 -0700

    move zero solution, there is a sliding window method worth pursuing

--- leetcode/283_move_zeros.py
+++ leetcode/283_move_zeros.py
@@ -11,14 +11,13 @@ def moveZeroes(self, nums: List[int]) -> None:
     """
     count = 0
     for i in range(len(nums)):
-        if nums[i] != 0 :
+        if nums[i] != 0:
             nums[count], nums[i] = nums[i], nums[count]
-            count +=1
+            count += 1
         # else:
         #     count +=1
     print(nums)
 
 
-
 input_1 = [0, 1, 0, 3, 12]
-p = moveZeroes(moveZeroes, input_1)
+moveZeroes(moveZeroes, input_1)
--- leetcode/two_sums.py
+++ leetcode/two_sums.py
@@ -5,32 +5,44 @@ Created on Sat Jan 18 08:56:46 2020
 @author: Kaushik Acharya
 """
 
+
 def twoSum(self, nums, target):
-        """
+    """
         :type nums: List[int]
         :type target: int
         :rtype: List[int]
         """
-        myList = nums
-#        myList.sort(reverse = False)
-#        print(myList)
-        
-        
-#        [i for (i,target-i) in enumerate(myList) if (target-i) in myList]
-#        foundList = [i for i, x in enumerate(myList) if x == (target-i)]
-#        print(foundList)
-        
-        for i in myList:
-            x = target-i
-            foundList = [j for j, val in enumerate(myList) if val == x]
-            print(foundList)
-            
-            
-            for k in foundList:
-                if myList.index(i)!= k:
-                    return myList.index(i), k
-#        for i = 0; i 
+    myList = nums
+    #        myList.sort(reverse = False)
+    #        print(myList)
 
-        
-p = twoSum(twoSum,[3,3],6)
-print (p)
+    #        [i for (i,target-i) in enumerate(myList) if (target-i) in myList]
+    #        foundList = [i for i, x in enumerate(myList) if x == (target-i)]
+    #        print(foundList)
+
+    for i in myList:
+        x = target - i
+        foundList = [j for j, val in enumerate(myList) if val == x]
+        print(foundList)
+
+        for k in foundList:
+            if myList.index(i) != k:
+                return myList.index(i), k
+
+
+#        for i = 0; i
+
+# trying to use a map to solve
+# def twoSum(self, nums, target):
+#     arr = []
+#     for i in range (len(nums)):
+#       arr.append(target-nums[i])
+#     print (arr)
+#
+#     for item in arr:
+#         for item in nums:
+#             return nums.index(x,y)
+
+
+p = twoSum(twoSum, [3, 3], 6)
+print(p)
